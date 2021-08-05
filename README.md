242aebd127f165b49bfed70fe06289a59b1af757
jverzani <jverzani@gmail.com>
2021-08-05 14:11:28 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CommonEq
+
+Package to share names for equality and inequality different from `==`, `<=`, ....
+
+Export `Eq`, `Lt`, `Le`, `Ge`, `Gt`, `Neq` and unicode equivalents `⩵` ( `\Equal[tab]`), `≪` (`\ll[tab]`), `≦` (`\leqq[tab]`), `≧` (\geqq[tab]`), `≫` (`\gg[tab]`).
--- src/CommonEq.jl
+++ src/CommonEq.jl
@@ -1,5 +1,44 @@
 module CommonEq
 
-# Write your package code here.
+export  Lt, ≪, Le, ≦, Eq, ⩵, Neq, Ge, ≫, Gt, ≧
+
+
+"Lt"
+Lt() = nothing
+
+"Le"
+Le() = nothing
+
+"Eq"
+Eq() = nothing
+
+"Neq"
+Neq() = nothing
+
+"Ge"
+Ge() = nothing
+
+"Gt"
+Gt() = nothing
+
+"≪, `\\ll[tab]` or `Lt`"
+const ≪  = Lt
+
+"≦, `\\leqq[tab]` or `Le`"
+const ≦ = Le
+
+"⩵, `\\Equal[tab]` or `Eq`"
+const ⩵ = Eq
+
+
+"≧, \\geqq[tab]` or `Ge`"
+const ≧ = Ge
+
+"≫, \\gg[tab] or `Gt`"
+const ≫ = Gt
+
+
+
+
 
 end
